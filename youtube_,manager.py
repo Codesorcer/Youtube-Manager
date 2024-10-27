6715cdd6dbb4acaa8b2dbc452d73c3d12c91e50f
Codesorcer <Rudraprataps7789@gmail.com>
2024-10-27 12:11:01 +0530

    first commit

---
+++ youtube_,manager.py
@@ -0,0 +1,10 @@
+
+
+while True:
+    print("\n Youtube Manager")
+    print("1. List all favourite videos")
+    print("2. Add a youtube video")
+    print("3. Update a youtube video details")
+    print("4. Delete a youtube video")
+    print("5. Exit the app")
+    input("Enter your choice")
